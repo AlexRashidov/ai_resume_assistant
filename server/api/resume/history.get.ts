774653588e27b75e2aa107ec24321aa5a3cda557
AlexRashidov <rashidov.sasha@yandex.ru>
2026-02-22 17:02:54 +0700

    comment smth about useAuth

--- server/api/resume/history.get.ts
+++ server/api/resume/history.get.ts
@@ -1,9 +1,9 @@
 import { defineEventHandler, createError } from 'h3'
-import useAuth from '../../middleware/useAuth.ts.bak'
+//import useAuth from '../../middleware/useAuth.ts.bak'
 import prisma from '../../db/client' // <-- default import
 
 export default defineEventHandler(async (event) => {
-    await useAuth(event) // ✅ функция middleware
+ //   await useAuth(event) // ✅ функция middleware
 
     const userId = event.context.userId
     if (!userId) throw createError({ statusCode: 401, message: 'Unauthorized' })
